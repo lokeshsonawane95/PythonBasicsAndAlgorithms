37cbce866754df7a12bf3c2a5056c40e7157c2ff
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-07-29 15:50:31 +0530

    Added to BasicsAndAlgorithms

---
+++ bubble_sort.py
@@ -0,0 +1,34 @@
+# bubble sort logic
+def bubble_sort(a):
+    """
+    sort elements in array using bubble sort
+    :param a: Array
+    :return: Sorted Array
+    """
+    try:
+        # loop till less than length of array a - 1
+        for i in range(len(a) - 1):
+            swapped = False
+            for j in range(len(a) - i - 1):
+
+                # comparing if current element is greater than next element or not
+                if a[j] > a[j + 1]:
+                    # swap logic
+                    a[j], a[j + 1] = a[j + 1], a[j]
+                    swapped = True
+
+            # if no element in the list is swapped then break the loop
+            if not swapped:
+                break
+        return a
+    except Exception as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    try:
+        array = list(map(int, input("Enter integer separated by spaces : ").strip().split()))
+        print("Array is : ", array)
+        print("Bubble sorted array is : ", bubble_sort(array))
+    except Exception as e:
+        print(e)
